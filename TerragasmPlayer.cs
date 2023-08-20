5c189a2abccc5048fe6fad3c7099066330562d98
unknown <feh.omori@gmail.com>
2023-08-20 02:53:44 -0300

    feat: migrate methods due to tModLoader version update

--- TerragasmPlayer.cs
+++ TerragasmPlayer.cs
@@ -2,19 +2,20 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
+using static Terraria.Player;
 
 namespace Terragasm;
 
 public class TerragasmPlayer : ModPlayer
 {
-    public override void OnEnterWorld(Player player)
+    public override void OnEnterWorld()
         => Task.Run(() => DeviceController.ConnectAsync());
 
     public override void OnHitAnything(float x, float y, Entity victim)
         => Task.Run(() => DeviceController.VibrateAsync(0.25, 500));
 
-    public override void Hurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit, int cooldownCounter)
-        => Task.Run(() => DeviceController.VibrateAsync(crit ? 1 : 0.5, 500));
+    public override void OnHurt(HurtInfo info)
+        => Task.Run(() => DeviceController.VibrateAsync(1, 500));
 
     public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
         => Task.Run(() => DeviceController.VibrateAsync(1, Player.respawnTimer * 1000 / 60));
